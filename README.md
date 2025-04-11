f56fa2665bcfef82f53c805714a329faf0a0709b
R. Neto <netolbv@gmail.com>
2025-04-10 22:43:52 -0300

    add controler health

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # java-cwhs
+
+Projeto java de teste para comunicação com HubSpot
+
+
+### Paths
+- http://localhost:8080/health
+Path para avaliar saúde do serviço(se on), pode ser add versão ou outra referência útil.
+
+- http://localhost:8080/monitor
+
+
+### Comandos úteis
+
+<code>mvn clean install</code>
+<code>mvnw spring-boot:run</code>
+
+### Links úteis
---
+++ src/main/java/com/oten/test/controllers/HealthController.java
@@ -0,0 +1,19 @@
+package com.oten.test.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        Map<String, String> map = new HashMap<>();
+        map.put("status", "ok");
+        return map;
+    }
+}
